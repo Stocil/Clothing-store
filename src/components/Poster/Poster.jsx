b70a0f3d00cbe0110f97e3b4a3e46b0d1d7bcff0
Stocil <vashurov02@mail.ru>
2024-03-04 03:37:23 +0300

    style: fix poster adaptive

--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -7,17 +7,14 @@ export function Poster() {
         variant="h1"
         position="absolute"
         sx={{ transform: { xs: "translateX(-50%)", sm: "none" } }}
-        top={{ xs: "240px", sm: "150px" }}
+        top={{ xs: "80px", sm: "150px" }}
         left={{ xs: "55%", sm: "calc(50% - 250px)" }}
-        fontSize={{ xs: "4rem", ss: "5rem", sm: "6rem" }}
+        fontSize={{ xs: "5rem", sm: "6rem" }}
       >
         Spark store
       </Typography>
 
-      <img
-        style={{ width: "100%", height: "400px" }}
-        src="../../../assets/bg1.png"
-      ></img>
+      <img className="poster__image" src="../../../assets/bg1.png"></img>
     </Box>
   );
 }
