b57bde333be026891f29e9befcc1d9023b7a1c5d
Stocil <vashurov02@mail.ru>
2024-03-09 17:00:32 +0300

    test deploy 5

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 import "./main.scss";
 import { store } from "./store";
-import { Theme } from "./style/theme";
+import { Theme } from "./style/Theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
